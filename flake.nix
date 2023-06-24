feb0869be2ab37fffaa832cba6f7908701781426
Break Yang <breakds@gmail.com>
2023-06-24 11:22:57 -0700

    Complete the script

--- flake.nix
+++ flake.nix
@@ -16,6 +16,15 @@
             import gi
             gi.require_version("FPrint", "2.0")
             from gi.repository import FPrint
+
+            ctx = FPrint.Context()
+
+            for dev in ctx.get_devices():
+                print(f"{dev} {dev.props.device_id}: Drive = ({dev.get_driver()})")
+                dev.open_sync()
+                dev.clear_storage_sync()
+                print(f"[ok] All prints deleted for {dev.props.device_id}")
+                dev.close_sync()
           '';
         })
       ];
